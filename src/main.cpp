23b05665aa58f8879c555d1565263138bf7e3e3e
Vincent Chang <vchan019@ucr.edu>
2015-04-16 21:10:49 -0700

    Worked a little on command runner

--- src/main.cpp
+++ src/main.cpp
@@ -29,15 +29,17 @@ void parseCommands(string& commandStr, vector<string>& commands)	{
 void runCommands(vector<string>& commands)	{ //runs the parsed vector of commands
 	//remember to parse || and &&
 	int numCommands = commands.size();
+	vector<string> commandBlock; //Stores one instruction then gets wiped later1
 	for(int i = 0; i < numCommands; ++i)	{
 		if((commands.at(i).equals(";") || commands.at(i).equals("&&") || commands.at(i).equals("||")	{
 			pid_t process = fork();
 				if(process == 0)	{ //Child process branch
-
+					
 				}
 
 				else if(process <= -1)	{ //Fork failed
-
+					perror("ERROR IN CREATING FORK");
+					exit(1);
 				}
 
 				else	{ //parent process
@@ -57,7 +59,7 @@ int main(int argc, char *argv[])	{
 		prompt();
 		getline (cin, input);
 		parseCommands(input, cmds);
-		
+		runCommands(cmds);	
 	}
 
 }
