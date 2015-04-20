de240c6b08919fb89de9d536ed012d0142a8a100
Vincent Chang <vchan019@ucr.edu>
2015-04-19 19:28:09 -0700

    Fixed || and && connectors

--- src/main.cpp
+++ src/main.cpp
@@ -14,32 +14,82 @@ void prompt()	{  //Add on name functionality later
 
 }
 
-void parseCommands(string& commandStr, vector<string>& commands)	{
-	char_separator<char> delim(";", "#"); //things that separate commands
-	tokenizer< char_separator<char> > token(commandStr, delim);
-
-	for(auto it = token.begin(); it!= token.end(); ++it)	{ //loop fills vector with commands
-		if (*it == "#")	{
-			commands.push_back("#");
-			break;
-		}
-		else	{
-			commands.push_back(*it);
-		}
+void parseCommands(string& commandStr)	{
+	char* filter = strtok(&commandStr[0], ";");
+	while(filter)	{
+		parseConnectors(filter, 0, 0);
 	}
-	commands.push_back("end"); //Will be used in the runCommands function to signify end of command chain
+}
+
+int parseConnectors(string& command, int orChecker, int andChecker) {  // 0 = fail, 1 = success, 2 = skip
+	const char* andLocation = strstr(command.c_str(), "&&");
+	const char* orLocation = strstr(command.c_str(), "||");
+	if(andLocation && orLocation && orLocation < andLocation)
+		andLocation = 0; 
+	
+	if(andLocation !- 0)	{
+		orChecker = parseConnectors(input.substr(0, andLocation - input.c_str()), crChecker, andChecker); //command succeeded
+		if(orChecker == 1)
+			return parseConnectors(andLocation + 2, 1, 0); 
+		else	{ //Command failed
+			return parseConnectors(andLocation + 2, 2, 0);
+		} 
+	}
+	
+	else if(orLocation != 0)	{ 
+		andChecker = parseConnectors(input.substr(0, orLocation - input.c_str()), orChecker, andChecker);
+		if(andChecker == 1)
+			parseConnectors(orLocation + 2, 0, 2); 
+		else	{ 
+			parseConnectors(orLocation + 2, 0, 1); 
+		}													}
+	
+	else	{ 
+		if(andChecker == 2)
+			return 0; 
+		if(orChecker == 2)
+			return 0;
+		return execCommand(command);
+	}
+	return 0;
+}
+
+
+bool execCommand(string& input)	{
+	
+	if (input == "exit")	
+		exit(0);
+	int process = fork();
+	if (process == 0)	{
+		char* args[2048];
+		vector<string> arg;
+
+	}
 }
 
 void runCommands(vector<string>& commands)	{ //runs the parsed vector of commands
 	//remember to parse || and &&
+	int execvpStatus = 0;
 	int numCommands = commands.size();
-	vector<string> commandBlock; //Stores one instruction then gets wiped later1
+	char** commandBlock = new char*[numCommands]; //c-string of commands
 	for(int i = 0; i < numCommands; ++i)	{
 		if((commands.at(i).compare(";") || commands.at(i).compare("&&") || commands.at(i).compare("||")))	{
 
-			pid_t process = fork();
+			int process = fork();
 				if(process == 0)	{ //Child process branch
-					
+					//commandBlock[loc] = NULL;
+					execvpStatus = execvp(commandBlock[0], commandBlock);					
+					if(execvpStatus <= -1)	{
+						perror("ERROR IN EXECUTING COMMAND");
+						int j = i;
+						bool foundConnecter = false;
+						if (commands.at(j).compare("&&"))	
+							j++;
+						for (;j < numCommands; j++)	{
+							if (commands
+						}
+
+					}
 				}
 
 				else if(process <= -1)	{ //Fork failed
@@ -48,7 +98,10 @@ void runCommands(vector<string>& commands)	{ //runs the parsed vector of command
 				}
 
 				else	{ //parent process
-
+					int waitStatus = waitpid(process, &execvpStatus, 0);
+					if (waitStatus <= -1)	
+						perror("ERROR IN WAIT");
+					
 				}
 		}
 	}
@@ -56,19 +109,18 @@ void runCommands(vector<string>& commands)	{ //runs the parsed vector of command
 
 
 
-
 int main(int argc, char *argv[])	{
-	while(1)	{
+//	while(1)	{
 		string input;
 		vector<string> cmds;
 		prompt();
 		getline (cin, input);
 		parseCommands(input, cmds);
 		for (auto it = cmds.begin(); it != cmds.end(); ++it)	{
-			cout << *it << " ";
-
+			cout << *it << "\n";
 		}
+		cout << "PARSED COMMANDS SIZE: " << cmds.size() << endl;
 		runCommands(cmds);	
-	}
+//	}
 
 }
